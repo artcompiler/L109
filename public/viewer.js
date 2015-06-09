ed032535151bb376aa28d0bf04005960ee5d0f86
Jeff Dyer <jeffdyer@acm.org>
2015-06-09 17:10:24 -0600

    Context menus

--- public/viewer.js
+++ public/viewer.js
@@ -409,7 +409,7 @@ window.exports.viewer = (function () {
         scaleItems();
 
         // Draw the menu
-        d3.select("g.item" + data.item)
+        d3.select(".item" + data.item)
           .append('g').attr('class', 'context-menu')
           .selectAll('tmp')
           .data(items).enter()
