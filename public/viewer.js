482e63a70e6c8b473f4e1e3b29bc701441e9505d
Jeff Dyer <jeffdyer@acm.org>
2015-06-10 12:03:30 -0600

    Context menus

--- public/viewer.js
+++ public/viewer.js
@@ -371,7 +371,7 @@ window.exports.viewer = (function () {
       .attr("transform", function(d) { return "translate(" + x(d.y) + "," + y(d.x) + ")"; })
       .attr("class", function(d) {
         if (d.item) {
-          return "item" + d.item;
+          return d.item;
         } else {
           return "parent";
         }
