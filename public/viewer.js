98c6f5b9cf8f9d894dd83f3505e3b7564e3601fe
Jeff Dyer <jeffdyer@acm.org>
2015-12-19 13:17:23 -0700

    Allow dash in tag names

--- public/viewer.js
+++ public/viewer.js
@@ -106,7 +106,8 @@ window.exports.viewer = (function () {
     var result = "";
     for (i = 0, len = str.length; i < len; i++) {
       code = str.charCodeAt(i);
-      if (!(code > 47 && code < 58) && // numeric (0-9)
+      if (!(code === 45) && // dash (-)
+          !(code > 47 && code < 58) && // numeric (0-9)
           !(code > 64 && code < 91) && // upper alpha (A-Z)
           !(code > 96 && code < 123)) { // lower alpha (a-z)
         return result;
